65cfabd5016af00b9b11012be52abf7983b27202
Dipesh Gautam <83548128+gdipesh@users.noreply.github.com>
2021-12-14 21:45:39 +0545

    update contact.js

--- contact.js
+++ contact.js
@@ -43,3 +43,25 @@ function ajax(method, url, data, success, error) {
   };
   xhr.send(data);
 }
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
